b1972bbd6e69198aa82d155d2216bf42f40c0087
godsneverdie <neverde616@gmail.com>
2025-11-20 02:44:48 +0530

    ll

--- linkedlist.py
+++ linkedlist.py
@@ -3,44 +3,34 @@ class Node:
         self.data=data
         self.next=None
 class linkedlist:
-    def __init__(self):
-        self.head=None
-    def disp(self):
+    def __init__(self,head=None):
+        self.head=head
+    def display(self):
         node=self.head
-        list=[]
-        while node:
-            print(node.data)
+        if node is None:
+            print("Empty List")
+            return
+        elements=[]
+        while node is not None:
+            elements.append(node.data)
             node=node.next
+        op=''
+        for i in elements:
+            op+='--->'+str(i)
+        print(op)
     def insert_at_start(self,data):
         node=Node(data)
         node.next=self.head
-        self.current=node
+        self.head=node
     def insert_at_end(self,data):
         node=Node(data)
         last=self.head
-        while last is not None:
+        while last.next is not None:
             last=last.next
         last.next=node
-    def delete_by_data(self,d):
-        current=self.head
-        if current is not None and current.data==d:
-            self.head=current.next
-            current.next=None
-        prev=None
-        while current is not None and current.data==d:
-            prev=current
-            current=current.next
-        prev.next=current.next
-        current=None    
-
-fn=Node(2)
-sn=Node(4)
-tn=Node(6)
 ll=linkedlist()
-ll.current=fn
-fn.next=sn
-sn.next=tn
 ll.insert_at_start(1)
-ll.insert_at_end(8)
-ll.delete_by_data(4)
-ll.disp()
+ll.insert_at_end(3)
+ll.insert_at_end(6)
+ll.insert_at_end(9)
+ll.display()
