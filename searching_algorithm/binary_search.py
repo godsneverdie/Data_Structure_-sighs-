66a74767a43fbf14957c2b00b70a9b382b6b56f3
godsneverdie <neverde616@gmail.com>
2025-11-23 15:32:03 +0530

    binary search

--- searching_algorithm/binary_search.py
+++ searching_algorithm/binary_search.py
@@ -1,8 +1,19 @@
 class arr:
     def __init__(self,arr):
         self.arr=arr
-        self.start=0
-        self.end=len(arr)-1
+        self.left=0
+        self.right=len(arr)-1
         
-        print(f"{self.start}--{self.end}\n{self.arr}")
+        print(f"Start Index = {self.left}\nEnd Index = {self.right}\narray: {self.arr}")
+    def binary_search(self,key):
+        while self.left<self.right:
+            mid=(self.left+self.right)//2
+            if self.arr[mid]==key:
+                return mid
+            if self.arr[mid]>key:
+                self.right=mid-1
+            if self.arr[mid]<key:
+                self.left=mid+1
+        return "Not found"
 a=arr([3,4,5,6,7,8,9,10])
+print(a.binary_search(9))
